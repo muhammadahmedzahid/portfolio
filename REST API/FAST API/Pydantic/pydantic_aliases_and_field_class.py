23d8abd1ab15451f5333908173249c1c190857f9
Muhammad Ahmed <muhammadahmed@bazoom.com>
2024-06-20 00:14:45 +0300

    Aliases and Field

---
+++ REST API/FAST API/Pydantic/pydantic_aliases_and_field_class.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from pydantic import ValidationError
+from pydantic import Field
+
+
+# Here Actually we are doing class inheritance.
+class Person(BaseModel):
+    first_name: str = Field(alias="First Name")
+    last_name: str = Field(alias="Last Name")
+    age: int = Field(alias="Age")
+
+
+data = {
+    "First Name": "Muhammad",
+    "Last Name": "Ahmed",
+    "Age": 15
+}
+p = Person.model_validate(data)
+print(p)
